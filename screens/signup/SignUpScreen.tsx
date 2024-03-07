d3bd3d57030267b3721762b3f456813fc32ce0e6
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-03-07 17:02:37 +0700

    finish page sign up

--- screens/signup/SignUpScreen.tsx
+++ screens/signup/SignUpScreen.tsx
@@ -2,6 +2,8 @@ import { View, Text, Image, Dimensions, TouchableOpacity, ScrollView } from 'rea
 import React, { ReactNode, useState } from 'react'
 import UserInput from '../../components/UserInput'
 import { useNavigation } from '@react-navigation/native'
+import { avatars } from '../../utils/utils'
+import { MaterialIcons } from '@expo/vector-icons'
 
 const SignUpScreen = () => {
     const navigation: any = useNavigation();
@@ -16,6 +18,8 @@ const SignUpScreen = () => {
     const handleChange = (name: string, text: string) => {
         setFormData({ ...formData, [name]: text });
     };
+
+    const [avatar, setAvatar] = useState(avatars[0]?.image.asset.url)
     return (
         <ScrollView className='h-full bg-white' >
             <View className='flex-1 items-center justify-start' >
@@ -23,6 +27,15 @@ const SignUpScreen = () => {
                 <View className='w-full h-full bg-white rounded-tl-[90px] -mt-44 flex items-center justify-start py-6 px-6 space-y-6' >
                     <Image className='w-16 h-16 ' resizeMode='contain' source={require('../../assets/images/logo.png')} />
                     <Text className='text-primaryText text-3xl font-semibold' > Join with us! </Text>
+
+                    <View className='w-full flex items-center justify-center relative -my-4' >
+                        <TouchableOpacity className='w-20 h-20 rounded-full border-2 bg-primary relative border-green-400' >
+                            <Image source={{ uri: avatar }} className='w-full h-full' resizeMode='contain' />
+                            <View className='h-6 w-6 bg-primary rounded-full absolute top-0 right-0 flex items-center justify-center' >
+                                <MaterialIcons name='edit' size={18} color={'white'} />
+                            </View>
+                        </TouchableOpacity>
+                    </View>
 
                     <View className='w-full flex items-center justify-center' >
                         <UserInput placeholder='Full Name' value={formData.fullName} onChangeText={(text) => handleChange('fullName', text)} isPass={false} />
